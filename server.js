0eb13ed40a9f0d547cef6b8f113eb7dfb601204b
rayGreycloud <rayGreycloud@gmail.com>
2016-10-24 12:19:05 -0700

    Add simple express routes

--- server.js
+++ server.js
@@ -3,7 +3,24 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Express');
+  // res.send('<h1>Hello from Express</h1>');
+  res.send({
+    name: 'Andrew',
+    likes: [
+      'Biking',
+      'Cities'
+    ]
+  });
+});
+
+app.get('/about', (req, res) => {
+    res.send('About page');
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+      errorMessage: 'Unknown request'
+    });
 });
 
 app.listen(3000);
