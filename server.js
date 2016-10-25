d780901bdf1ffc0fd37071c17196247267a5b399
rayGreycloud <rayGreycloud@gmail.com>
2016-10-25 13:51:15 -0700

    Add templates and hbs

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
+
+app.set('view engine', 'hbs');
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello from Express</h1>');
-  res.send({
-    name: 'Andrew',
-    likes: [
-      'Biking',
-      'Cities'
-    ]
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Welcome to Some Website'
   });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.get('/bad', (req, res) => {
