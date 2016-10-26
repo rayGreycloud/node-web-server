fa35acea5d8aa9324684956e54e5de134412e9fd
rayGreycloud <rayGreycloud@gmail.com>
2016-10-26 06:12:21 -0700

    Add partials for header and footer

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const hbs = require('hbs');
 
 const app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
